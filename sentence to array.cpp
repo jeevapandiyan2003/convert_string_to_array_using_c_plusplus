04fa62dcc30e2bf69a6113d7c2f6edf165e9ab65
JEEVAPANDIYAN S <132741228+jeevapandiyan2003@users.noreply.github.com>
2023-08-19 15:37:38 +0530

    Update sentence to array.cpp

--- sentence to array.cpp
+++ sentence to array.cpp
@@ -16,19 +16,19 @@ class ans
   {
     if(wrr[i]!=' ')
     {
-      a+=wrr[i];
+      a+=wrr[i];//when there is no scace we are adding up every character
     
     }
     else
     {
-      arr[b]=a;
+      arr[b]=a;//when we found the space between the two words we are puting the previously appended value to (a) to array 1
       b+=1;
       a="";
     }
     if(i==wrr.size())
     {
       
-      arr[b]=a;
+      arr[b]=a;//to put the final addup value to the array
     }
   }
  
@@ -40,12 +40,12 @@ class ans
 int main() 
 {
   string word;
-  getline(cin,word);
-  ans obj;
-  obj.split_ary(word);
+  getline(cin,word); //getting a sentence we have to use getline
+  ans obj;//creating object for class
+  obj.split_ary(word);//accessing the function in the class through object
   for (int i=0;i<obj.b+1;i++)
   {
-    cout<<obj.arr[i]<<" ";
+    cout<<obj.arr[i]<<" ";// printing the splited words which is kept inside the array
   }
 
   return 0;
